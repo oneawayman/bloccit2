2a3c8d4b96ca52e4b5f5d79a342b125f2ad32dff
oneawayman <yoavbauer@gmail.com>
2014-03-23 23:07:16 +0000

    Created post resource and updated index and show views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Bloccit2::Application.routes.draw do
-  get "welcome/index"
-  
-  get "welcome/about"
+
+  resources :posts
+
+  match "about", to: 'welcome#about', via: :get
   
   root to: 'welcome#index'
   end
